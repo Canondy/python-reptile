8f8c5d7ac8bc96cd23c0db9f03c5d22465f46120
liuhuan <1589328059@qq.com>
2022-05-23 20:50:48 +0800

    使用代理爬取

--- 003处理cookie及防盗链及代理和实战案例/003使用代理爬取.py
+++ 003处理cookie及防盗链及代理和实战案例/003使用代理爬取.py
@@ -1,2 +1,23 @@
 
-# 使用代理
+# 使用代理（目的防止自己的IP请求过多被封)
+
+import requests
+
+# 设置代理
+proxy = {
+    # 这个代理IP不可用，自行购买
+    "http": "http://202.109.157.61:9000"
+}
+
+
+# 设置爬取的网址
+url = "http://www.baidu.com"
+
+
+# 使用request.get()请求爬取
+res = requests.get(url, proxies=proxy, verify=False)
+
+# 处理返回结果乱码问题
+res.encoding = "utf-8"
+
+print(res.text)
