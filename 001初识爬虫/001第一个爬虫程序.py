d672c272cbde456cebb3eec095d71ed453d444a9
liuhuan <1589328059@qq.com>
2022-05-23 18:46:57 +0800

    修改爬取url和路径

--- 001初识爬虫/001第一个爬虫程序.py
+++ 001初识爬虫/001第一个爬虫程序.py
@@ -4,8 +4,8 @@
 from urllib.request import urlopen
 
 # 定义要爬取的网址
-# url = "http://www.baidu.com"
-url = "http://www.taobao.com"
+url = "http://www.baidu.com"
+# url = "http://www.taobao.com"
 
 # 调用urlopen方法，返回一个结果
 resp = urlopen(url)
@@ -13,5 +13,5 @@ resp = urlopen(url)
 # with open(文件名，模式，编码方式) 读写文件的方法
 # mode="r" 表示读文件   mode="w" 表示写文件
 # encoding="utf-8"  表示编码方式为utf-8
-with open("../url/baidu.html", mode="w", encoding="utf-8") as f:
+with open("url/baidu.html", mode="w", encoding="utf-8") as f:
     f.write(resp.read().decode("utf-8"))
