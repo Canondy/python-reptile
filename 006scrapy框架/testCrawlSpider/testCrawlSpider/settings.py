138f412c45e82463ccdd58c6c847d42f1d1143ad
liuhuan <1589328059@qq.com>
2022-05-29 23:55:38 +0800

    CrawlSpider全站爬取更换爬取地址

--- 006scrapy框架/testCrawlSpider/testCrawlSpider/settings.py
+++ 006scrapy框架/testCrawlSpider/testCrawlSpider/settings.py
@@ -14,10 +14,12 @@ NEWSPIDER_MODULE = 'testCrawlSpider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'testCrawlSpider (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
+
+LOG_LEVEL = "ERROR"
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- 006scrapy框架/testCrawlSpider/testCrawlSpider/spiders/sunSpider.py
+++ 006scrapy框架/testCrawlSpider/testCrawlSpider/spiders/sunSpider.py
@@ -6,19 +6,23 @@ from scrapy.spiders import CrawlSpider, Rule
 class SunspiderSpider(CrawlSpider):
     name = 'sunSpider'
     # allowed_domains = ['www.xxx.com']
-    start_urls = ['http://www.xxx.com/']
+    # 这个网站加了腾讯反爬机制，我这IP可能被封了
+    # start_urls = ['https://wz.sun0769.com/political/index/supervise?page=1']
+    start_urls = ['https://movie.douban.com/top250?start=25']
 
     # 连接提取器：根据指定的规则 （allow = "正则表达式"）进行指定链接的提取
-    link = LinkExtractor(allow=r'Items/')
+    link = LinkExtractor(allow=r'start=\d+')
 
     rules = (
         # 规则解析器：将链接提取器提取到的连接进行指定规则 （callback）的解析操作
+        # follow=True 作用：可以将链接提取器继续作用到  连接提取器提取到的链接所对应的页面中
         Rule(link, callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
+        print(response)
+        # item = {}
         #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
         #item['name'] = response.xpath('//div[@id="name"]').get()
         #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        # return item
